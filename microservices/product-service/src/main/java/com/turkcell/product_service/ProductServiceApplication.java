d68bb01da7de0c2367b938ead344ad61a537792c
Orhan Gölcür <orhangolcur0@gmail.com>
2026-06-10 19:11:47 +0300

    keycloak docker compose'a eklendi

--- microservices/product-service/src/main/java/com/turkcell/product_service/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/turkcell/product_service/ProductServiceApplication.java
@@ -2,7 +2,6 @@ package com.turkcell.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableScheduling
